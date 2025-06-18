37553220f69afd4e5505748f10e6f44637ee4bd7
MelinaNorton <melinan@clemson.edu>
2025-06-18 16:25:34 +0200

    refactored locations/references

--- my-project/src/components/locations.tsx
+++ my-project/src/components/locations.tsx
@@ -7,8 +7,8 @@ type locationsProps = {
 
 const LocationPic:React.FC<locationsProps> = (props : locationsProps) =>{
     return(
-        <div className="relative lg:h-94 h-85 2xl:h-150 2xl:w-180 lg:w-160 md:w-160 w-120">
-            <img src={props.bgSrc} className="flex rounded-xl lg:h-94 md:h-65 h-55 2xl:h-150 2xl:w-300 lg:w-200 md:w-300 w-60"></img>
+        <div className="flex flex-col items-center justify-center p-4">
+            <img src={props.bgSrc} className="flex rounded-xl lg:h-94 md:h-65 h-80 2xl:h-150 2xl:w-300 lg:w-200 md:w-300 w-90"></img>
             <div className="relative flex flex-col 2xl:w-150 lg:w-150 2-100 2xl:pt-0 lg:pt-0 pt-3">
                 <h1 className="2xl:absolute lg:absolute 2xl:lg lg:text-lg md:text-3xl text-lg font-bold tracking-wider text-white 2xl:bottom-18 lg:bottom-18 bottom-18 2xl:left-6 lg:left-6 left-6 2xl:pb-0 lg:pb-0 md:pb-6 pb-0">{props.title}</h1>
                 <p className="2xl:absolute lg:absolute text-xs font-semibold text-white/60 2xl:bottom-5 lg:bottom-5 bottom-5 2xl:left-6 lg:left-6 left-6">{props.desc}</p>
--- my-project/src/components/locations_references.tsx
+++ my-project/src/components/locations_references.tsx
@@ -30,9 +30,9 @@ const LocationsSection = () => {
     }
 
     return(
-        <main className="bg-black bg-cover h-min-screen flex flex-col lg:gap-y-10 gap-y-10 justify-start items-center relative overflow-x-clip font-sans p-5">
-            <div className="flex lg:flex-col 2xl:flex-col md:flex-col flex-col gap-y-2 gap-x-20 lg:gap-x-10 2xl:gap-x-20 2xl:w-4/10 lg:w-67/70 md:w-70/70 w-95">
-                <h1 className="lg:pr-10 lg:text-4xl md:text-3xl text-xl font-bold 2xl:pr-10 pr-12 2xl:pb-2 lg:pb-2 pb-0">Our Locations</h1>
+        <main className="bg-black bg-cover h-min-screen flex flex-col lg:gap-y-10 gap-y-10 justify-start items-center relative overflow-x-clip font-sans p-4">
+            <div className="flex lg:flex-col 2xl:flex-col md:flex-col flex-col gap-y-2 gap-x-20 lg:gap-x-10 w-full border border-red-500">
+                <h1 className="lg:pr-10 lg:text-4xl md:text-3xl text-xl font-bold pb-2">Our Locations</h1>
                 <div className="flex 2xl:flex-row lg:flex-row flex-col 2xl:gap-25 lg:gap-10">
                     <p className="lg:text-lg 2xl:text-md text-sm font-extralight text-white/60 pb-5">Strategically situated in the Republic of Kosova, South Eastern Europe, our location offers unparalleled advantages for companies seeking to optimize their operations through outsourcing. With a robust infrastructure and a skilled workforce, partnering with us enables businesses to capitalize on cost-effectiveness, access to a talented labor pool, and strategic geographic positioning for enhanced market reach in Europe and beyond.</p>
                     <div className="flex flex-row gap-3">
@@ -41,21 +41,21 @@ const LocationsSection = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row justify-center gap-x-8 2xl:pr-0 lg:pr-0 md:pr-30 pr-8">
-                <img src = "/resources/Location1.png" className=" rounded-xl lg:h-94 h-30 2xl:h-150 2xl:flex lg:flex md:hidden flex"></img>
+            <div className="flex flex-row justify-center items-center gap-x-8 w-screen lg:w-full border border-red-500">
+                <img src = "/resources/Location1.png" className=" rounded-xl lg:h-94 h-30 2xl:h-150 2xl:flex lg:flex hidden"></img>
                 <LocationPic bgSrc="/resources/BlankLocation2.png" title="Prishtina Headquarters" desc="Located in the heart of Prishtina, the SPEEEX HQ presents the most preeminent offices, equipped with the latest technological advancements and modern amenities. Along with its astonishing voew of the city, this facility is a marvel of work environment." lm="LEARN MORE"/>
-                <img src = "/resources/Location3.png" className="rounded-xl lg:h-94 h-30 2xl:h-150 2xl:flex lg:flex md:hidden flex"></img>
+                <img src = "/resources/Location3.png" className=" rounded-xl lg:h-94 h-30 2xl:h-150 2xl:flex lg:flex hidden"></img>
             </div>
-            <div className="flex lg:flex-row 2xl:flex-col md:flex-col flex-col gap-y-2 gap-x-20 lg:gap-x-10 2xl:gap-x-20 2xl:w-4/10 lg:w-69/70 md:w-70/70 w-95 2xl:pt-15 lg:pt-15 md:pt-25 pt-0">
-                <h1 className="lg:pr-10 lg:text-4xl md:text-3xl text-xl font-bold 2xl:pr-10 pr-10">References</h1>
+            <div className="flex lg:flex-row 2xl:flex-col md:flex-col flex-col gap-y-2 gap-x-20 lg:gap-x-10 2xl:gap-x-20 w-full">
+                <h1 className="lg:pr-10 lg:text-4xl md:text-3xl text-xl font-bold">References</h1>
                 <p className="lg:text-lg 2xl:text-md text-sm font-extralight text-white/60 pb-5"> Our dedication to achieving excellence is underscored by the accomplishments of our esteemed clientele. We have been privileged to provide services to a multitude of businesses across diverse industries and global markets, and we are gratified to have secured their trust and confidence.</p>
             </div>
-                <div className="flex flex-col gap-3">
-                    <div className="flex justify-start items-start 2xl:pl-0 lg:pl-2 md:pl-175 pl-118">
+                <div className="flex flex-col gap-3 border border-red-500 justify-center items-start w-full">
+                    <div className="flex justify-start items-start">
                         <p className="self-start tracking-widest text-indigo-200 font-semibold">International Clients</p>
                     </div>
-                    <div className="flex flex-col 2xl:gap-4 lg:gap-2 gap-2 2xl:pl-0 lg:pl-0 pl-10">
-                        <div className="flex flex-row lg:justify-center 2xl:justify-center md:justify-start 2xl:pl-0 lg:pl-0 md:pl-90 pl-0 justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4">
+                    <div className="flex flex-col 2xl:gap-4 2 gap-2 border border-green-500">
+                        <div className="flex flex-row lg:justify-center justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4">
                             {firstClients.map( (src,i) =>(
                                 <ClientImage
                                     key={i}
@@ -64,7 +64,7 @@ const LocationsSection = () => {
                                 />
                             ))}
                         </div>
-                        <div className="flex-row justify-center 2xl:gap-x-4 lg:gap-x-2 gap-x-2 2xl:flex lg:flex hidden">
+                        <div className="flex-row justify-center 2xl:gap-x-4 gap-x-2 2xl:flex lg:flex hidden border border-blue-500">
                             {secondClients.map( (src, i) =>(
                                 <ClientImage
                                     key={i}
@@ -75,13 +75,13 @@ const LocationsSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col gap-3 2xl:pl-0 lg:pl-0 pl-10">
-                    <div className="flex flex-row justify-start 2xl:pl-0 lg:pl-0 md:pl-90 pl-0 2xl:gap-86 lg:gap-70 gap-80">
-                        <p className="self-start tracking-widest text-indigo-200 font-semibold 2xl:pl-2 lg:pl-2 pl-55 md:pl-0">Local Clients</p>
+                <div className="flex flex-col gap-3 border border-red-500 justify-center items-start w-full">
+                    <div className="flex flex-row justify-start 2xl:gap-86 lg:gap-70 gap-80">
+                        <p className="self-start tracking-widest text-indigo-200 font-semibold">Local Clients</p>
                         <p className="self-start tracking-widest text-indigo-200 font-semibold 2xl:flex lg:flex md:hidden hidden">International Partners</p>
                     </div>
-                    <div className="flex 2xl:flex-row lg:flex-row md:flex-col flex-col gap-y-4">
-                        <div className="flex flex-row lg:justify-center 2xl:justify-center justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4 2xl:pl-0 lg:pl-0 md:pl-90 pl-54 2xl:pr-4 lg:pr-2">
+                    <div className="flex flex-col 2xl:gap-4 2 gap-2 border border-green-500 max-w-none w-max">
+                        <div className="flex flex-row 2xl:justify-center justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4 pr-2">
                             {localClients.map((src,i) => (
                                 <ClientImage
                                     key={i}
@@ -90,8 +90,8 @@ const LocationsSection = () => {
                                     />
                                 ))}
                         </div>
-                        <p className="self-start tracking-widest text-indigo-200 font-semibold 2xl:hidden lg:hidden md:flex flex md:pl-90 pl-55">International Partners</p>
-                        <div className="flex flex-row lg:justify-center 2xl:justify-center justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4 2xl:pl-0 lg:pl-0 md:pl-90 pl-0">
+                        <p className="self-start tracking-widest text-indigo-200 font-semibold lg:hidden">International Partners</p>
+                        <div className="flex flex-row justify-start 2xl:gap-x-4 lg:gap-x-2 gap-x-4">
                             {intPartners.map((src,i) => (
                                 <ClientImage
                                     key={i}
