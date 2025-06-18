2a3e338ac297315c5ed728a962a11599679f40af
MelinaNorton <melinan@clemson.edu>
2025-06-18 09:15:21 +0200

    fixed locations card spacing issue lg:screen

--- my-project/src/components/locations.tsx
+++ my-project/src/components/locations.tsx
@@ -7,7 +7,7 @@ type locationsProps = {
 
 const LocationPic:React.FC<locationsProps> = (props : locationsProps) =>{
     return(
-        <div className="relative lg:h-94 h-85 2xl:h-150 2xl:w-180 lg:w-180 w-120">
+        <div className="relative lg:h-94 h-85 2xl:h-150 2xl:w-180 lg:w-160 w-120">
             <img src={props.bgSrc} className=" rounded-xl lg:h-94 h-55 2xl:h-150"></img>
             <div className="relative flex flex-col 2xl:w-150 lg:w-150 2-100 2xl:pt-0 lg:pt-0 pt-3">
                 <h1 className="2xl:absolute lg:absolute 2xl:lg lg:text-lg text-lg font-light tracking-wider text-white 2xl:bottom-18 lg:bottom-18 bottom-18 2xl:left-6 lg:left-6 left-6">{props.title}</h1>
